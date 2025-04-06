f4fdaddea9a10e7447a572be9cab5666277607f6
qAbdullajon <azikmelor7705@gmail.com>
2025-04-07 03:53:54 +0500

    was

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Static export uchun
+  output: 'standalone',
   images: {
     unoptimized: true, // Static exportda
   },
