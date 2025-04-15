2b0177c10ce6edcac0a6bfad46f0dcc8198d489c
qAbdullajon <azikmelor7705@gmail.com>
2025-04-15 15:14:50 +0500

    qwzx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Script from "next/script"; 
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const montserrat = Montserrat({
   variable: "--font-geist-sans",
@@ -14,31 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-GH8NCEDTX5"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-GH8NCEDTX5');
-            `,
-          }}
-        />
-      </head>
       <body
         className={`${montserrat.variable} antialiased`}
-        style={{ fontFamily: "'Arial', sans-serif" }}
+        style={{ fontFamily: "'Arial', sans-serif" }} // Arialni qo'shish
         suppressHydrationWarning
       >
         {children}
       </body>
+        <GoogleTagManager gtmId="G-GH8NCEDTX5" />
     </html>
   );
 }
