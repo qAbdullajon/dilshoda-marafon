8d5280eef17de97a0c1b269a1e8e100aa8a9fb92
qAbdullajon <azikmelor7705@gmail.com>
2025-04-16 22:19:09 +0500

    Qwas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import { Head } from "next/document";
+import Head from "next/head";
 import Script from "next/script";
 
 const montserrat = Montserrat({
@@ -16,26 +16,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        {/* Facebook Pixel Script */}
-        <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1088463105874255');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        <title>My Website</title>
       </Head>
+
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1088463105874255');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+
       <body
         className={`${montserrat.variable} antialiased`}
         style={{ fontFamily: "'Arial', sans-serif" }}
